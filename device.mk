6381c7ad8297590b9b9fac185b5c88025e8b723e
Firmank123 <firmankediri123@gmail.com>
2026-02-20 18:15:14 +0000

    p
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+include vendor/private/keys/keys.mk
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
